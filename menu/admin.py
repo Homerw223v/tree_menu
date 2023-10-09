6300fba04794cd98e5ef91c61e9d5fb9dc36c232
Federov Renat <homerw223v@mail.ru>
2023-10-09 15:32:38 +0700

    Create Meni in Admin

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Menu
+
+
+@admin.register(Menu)
+class TreeMenuAdmin(admin.ModelAdmin):
+    fields = ['name', 'slug', 'parent']
+    list_display = ['__str__', 'url', 'parent']
+    prepopulated_fields = {'slug': ('name',)}
+
+    class Meta:
+        ordering = ['parent']
