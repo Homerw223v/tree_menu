f46aa13d86211ead789e4cf873f5be176bd4de5e
Federov Renat <homerw223v@mail.ru>
2023-10-09 21:33:20 +0700

    Fix

--- menu/templatetags/create_menu.py
+++ menu/templatetags/create_menu.py
@@ -1,43 +1,47 @@
 from django import template
-from django.db.models import QuerySet
 
 from ..models import Menu
 
 register = template.Library()
-base_url = str()
-current_url = str()
 
 
-def check_set_url() -> None:
+def check_set_url(current_url: str, base_url: str) -> str | None:
     """Create new current url
-    :rtype: None"""
-    global current_url, base_url
-    url_now_len: int = len(current_url.split('-'))
-    base_url_len: int = len(base_url.split('-'))
+    :param current_url:
+    :type current_url: str
+    :param base_url:
+    :type base_url: str
+    :rtype: dict | None
+    """
+    url_now_len = len(current_url.split('-'))
+    base_url_len = len(base_url.split('-'))
     if url_now_len == base_url_len:
-        current_url = '-'.join((base_url.split('-')[:url_now_len]))
+        return '-'.join(base_url.split('-')[:url_now_len])
     elif url_now_len < base_url_len:
-        current_url = '-'.join((base_url.split('-')[:url_now_len + 1]))
+        return '-'.join(base_url.split('-')[:url_now_len + 1])
     else:
-        current_url = None
+        return None
 
 
 @register.inclusion_tag('menu/menu.html')
-def children(query: list) -> dict:
+def children(query: list, current_url: str, base_url: str) -> dict:
     """Create children menu if need
     :param query:
     :type query: list
+    :param current_url:
+    :type current_url: str
+    :param base_url:
+    :type base_url: str
     :rtype: dict
     """
-    global base_url, current_url
-    menu: list = list()
-    left: list = list()
+    menu = []
+    left = []
     for table in query:
         if table.parent.url == current_url:
             menu.append(table)
         else:
             left.append(table)
-    check_set_url()
+    current_url = check_set_url(current_url, base_url)
     return {'left': left, 'main_menu': menu, 'menu_url': current_url, 'base': base_url}
 
 
@@ -46,14 +50,13 @@ def draw_menu(url: str) -> dict:
     """Create base menu
     :param url:
     :type url: str
-    :return:
+    :rtype: dict
     """
-    global current_url, base_url
     base_url = url
-    current_url = None if base_url == '' else base_url.split('-')[0]
-    menu: QuerySet = Menu.objects.select_related('parent').order_by('name')
-    main_menu: list = []
-    left: list = []
+    current_url = '' if base_url == '' else base_url.split('-')[0]
+    menu = Menu.objects.select_related('parent').order_by('name')
+    main_menu = []
+    left = []
     if menu:
         for table in menu:
             if table.parent is None:
@@ -61,4 +64,4 @@ def draw_menu(url: str) -> dict:
             else:
                 left.append(table)
         return {'main_menu': main_menu, 'left': left, 'base': base_url, 'menu_url': current_url}
-    return dict()
+    return {}
